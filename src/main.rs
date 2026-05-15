b86b05dd4f5e2609cc8138206196033893640432
moyashi170607 <96937973+moyashi170607@users.noreply.github.com>
2026-05-16 02:50:04 +0900

    ウィンドウサイズの変更

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ use crate::app::NowPlayingApp;
 fn main() {
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0]),
+            .with_inner_size([400.0, 250.0])
+            .with_min_inner_size([300.0, 200.0]),
         ..Default::default()
     };
     eframe::run_native(
